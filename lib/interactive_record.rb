a5569cd44117fadcc93f71df4b797f9a4d4eb918
Rachele Suy <rachelesuy@gmail.com>
2019-06-18 18:33:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -39,17 +39,14 @@ class InteractiveRecord
        values << "'#{send(col_name)}'" unless send(col_name).nil?
      end
      values.join(", ")
-   end
+  end
+  
+  def save
+    sql = "INSERT INTO #{table_name_for_insert}"
+  end
 end
 
-# def values_for_insert
-#     values = []
-#     self.class.column_names.each do |col_name|
-#       values << "'#{send(col_name)}'" unless send(col_name).nil?
-#     end
-#     values.join(", ")
-#   end
-
-#   def col_names_for_insert
-#     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+# #    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+#     DB[:conn].execute(sql)
+#     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
 #   end
